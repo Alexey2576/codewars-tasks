72c5e38ed40022b72f827eb0ac76e10636ae5888
Alexey2576 <alexeynikinitin@yandex.by>
2021-10-31 00:47:39 +0300

    Grasshopper - Summation

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-var summation = function (num) {
-  let sum = 0;
-  for(let i = 1; i <= num; i++) {
-    sum += i;
-  }
-  return sum;
+function accum(s) {
+  let str = s.toLowerCase().split("");
+  let i = 0;
+  let newStr = "";
+  str.forEach((el) => {
+    i++;
+    newStr = newStr + "-";
+    for (let a = 0; a < i; a++) {
+      if (a === 0)
+        newStr += el.toUpperCase();
+      else
+        newStr += el.toLowerCase();
+    }
+  })
+  return console.log(newStr.substr(1))
 }
+
+accum("ZpglnRxqenU");
