2ba76dd41c77e6acddddc8f6ec0ff3b78610d58e
Alexey2576 <alexeynikinitin@yandex.by>
2021-11-01 21:48:06 +0300

    Remove the minimum

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-function squareDigits(num){
-  let newNum = "";
-  let mass = num.toString().split("");
-  let newMass = mass.map((el) => Math.pow(parseInt(el),2))
-  newMass.forEach((item, newMass) => {
-    newNum = newNum.concat(item);
-  })
-  console.log(parseInt(newNum));
+function removeSmallest(numbers) {
+  let minEl = numbers[0];
+  for (let i = 0; i < numbers.length; i++) {
+    if (minEl > numbers[i])
+      minEl = numbers[i];
+  }
+  let newArr = numbers.slice(0);
+  newArr.splice(newArr.indexOf(minEl), 1);
+  return console.log(newArr);
 }
 
-squareDigits(9112);
+removeSmallest([1, 2, 3, 4, 5]);
+removeSmallest([5, 3, 2, 1, 4]);
+removeSmallest([5, 1, 2, 1, 4]);
+removeSmallest([2, 2, 1, 2, 1]);
+removeSmallest([]);
