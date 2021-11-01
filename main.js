8ca49ad23490128e8100e7f4606cba28d274b697
Alexey2576 <alexeynikinitin@yandex.by>
2021-11-01 19:29:12 +0300

    Square Every Digit

--- main.js
+++ main.js
@@ -1,37 +1,11 @@
-function longestConsec(strarr, k) {
-  if (k > 1 && k <= strarr.length) {
-    let mass = [];
-    for (let a = 0; a < strarr.length; a++) {
-      let string = "";
-      for (let i = a; i < k; i++) {
-        if (k > strarr.length)
-          {
-            let maxStr = mass[0];
-            for (let x = 0; x < mass.length; x++) {
-              if (maxStr.length < mass[x].length)
-                maxStr = mass[x];
-            }
-            return console.log(maxStr);
-          }
-        else
-          string = string.concat(strarr[i]);
-      }
-      k++;
-      mass.push(string);
-    }
-  }
-  else if (k === 1)
-    {
-      let maxStr = strarr[0];
-      for (let x = 0; x < strarr.length; x++) {
-        if (maxStr.length < strarr[x].length)
-          maxStr = strarr[x];
-      }
-      return console.log(maxStr);
-    }
-  else
-    return "";
+function squareDigits(num){
+  let newNum = "";
+  let mass = num.toString().split("");
+  let newMass = mass.map((el) => Math.pow(parseInt(el),2))
+  newMass.forEach((item, newMass) => {
+    newNum = newNum.concat(item);
+  })
+  console.log(parseInt(newNum));
 }
 
-
-longestConsec(["zone", "abigail", "thetaaaaaaaaa", "formm", "libennnnn", "zass"], 2)
+squareDigits(9112);
