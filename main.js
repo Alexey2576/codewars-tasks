5750d24beffecc0396e9dd28ba9b4a3f5bcf5487
Alexey2576 <alexeynikinitin@yandex.by>
2021-11-01 18:59:00 +0300

    Consecutive strings

--- main.js
+++ main.js
@@ -1,18 +1,38 @@
-function accum(s) {
-  let str = s.toLowerCase().split("");
-  let i = 0;
-  let newStr = "";
-  str.forEach((el) => {
-    i++;
-    newStr = newStr + "-";
-    for (let a = 0; a < i; a++) {
-      if (a === 0)
-        newStr += el.toUpperCase();
-      else
-        newStr += el.toLowerCase();
+function longestConsec(strarr, k) {
+  if (k > 1 && k <= strarr.length) {
+    let mass = [];
+    for (let a = 0; a < strarr.length; a++) {
+      let string = "";
+      for (let i = a; i < k; i++) {
+        if (k > strarr.length)
+          {
+            let c = 0;
+            let maxStr = mass[0].length;
+            for (let x = 0; x < mass.length; x++) {
+              if (maxStr < mass[x].length)
+                c = x;
+            }
+            return console.log(mass[c]);
+          }
+        else
+          string = string.concat(strarr[i]);
+      }
+      k++;
+      mass.push(string);
     }
-  })
-  return newStr.substr(1);
+  }
+  else if (k === 1)
+    {
+      let maxStr = strarr[0];
+      for (let x = 0; x < strarr.length; x++) {
+        if (maxStr.length < strarr[x].length)
+          maxStr = strarr[x];
+      }
+      return console.log(maxStr);
+    }
+  else
+    return "";
 }
 
-accum("ZpglnRxqenU");
+
+longestConsec(["zoneeeeeeeeeee", "abigail", "theta", "form", "libe", "zasssssss"], 6)
