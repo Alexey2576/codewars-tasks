277c19afe6f573f2d2505f20f486dc64b7d7e413
Alexey2576 <alexeynikinitin@yandex.by>
2021-11-02 20:28:24 +0300

    Profile Lookup

--- main.js
+++ main.js
@@ -1,11 +1,46 @@
-function sum(arr, n) {
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
   // Only change code below this line
-  if (n <= 0) {
-    return 0;
-  } else {
-    return console.log(sum(arr, n - 1) + arr[n - 1]);
+  let p = ""
+  for (let i = 0; i < contacts.length; i++) {
+    if (contacts[i]["firstName"] === name) {
+      if (contacts[i].hasOwnProperty(prop))
+        p = contacts[i][prop];
+      else
+        p = "No such property"
+    }
   }
+  if (p === "")
+    p = "No such contact"
+  return p;
   // Only change code above this line
 }
 
-sum([1,2,3,4,5], 3)
+lookUpProfile("Akira", "likes");
