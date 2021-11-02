1da7d9ef403a597e145bcbea066cad46f4849510
Alexey2576 <alexeynikinitin@yandex.by>
2021-11-02 21:25:50 +0300

    Find the stray number

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-function sumTwoSmallestNumbers(numbers) {
-  compare = (a, b) => {
+function stray(numbers) {
+  numbers.sort((a, b) => {
     if (a < b)
-      return -1
-    if (a === b)
-      return 0;
+      return -1;
     if (a > b)
       return 1;
-  }
-  numbers.sort(compare);
-  return console.log(`Sum should be ${numbers[0] + numbers[1]}`)
+    if (a === b)
+      return 0
+  })
+  if (numbers[0] === numbers[1])
+    return numbers[numbers.length-1]
+  else
+    return numbers[0];
 }
-
-sumTwoSmallestNumbers([5, 8, 12, 19, 22])
-sumTwoSmallestNumbers([15, 28, 4, 2, 43])
