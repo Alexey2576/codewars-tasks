955b6fa57fc1228d1280a817dc2dfa85e400016c
Alexey2576 <alexeynikinitin@yandex.by>
2021-11-03 18:52:14 +0300

    Dashatize it

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
-function tribonacci(signature,n){
-  let sum = 0;
-  if (n === 0)
-    return console.log([]);
-  else if (n < 3) {
-    let mass = [];
-    for (let i = 0; i < n; i++) {
-      mass.push(signature[i]);
+function dashatize(num) {
+  let str = "";
+  let mass = num.toString().split("");
+  mass.forEach(el => {
+    if ((parseInt(el) % 2) > 0) {
+      if (str.substr(str.length-1, 1) === "-")
+        str = str + el + "-";
+      else
+        str = str + "-" + el + "-";
     }
-    return console.log(mass);
+    else
+      str = str + el;
+  })
+  if (str.substr(0, 1) === "-") {
+    str = str.substr(1, str.length-1);
   }
-  else {
-    for (let i = 2; i < (n-1); i++) {
-      sum = signature[i] + signature[i-1] + signature[i-2];
-      signature.push(sum);
-    }
-    return console.log(signature);
+  if (str.substr(str.length-1, 1) === "-") {
+    str = str.substr(0, str.length-1);
   }
+  return console.log(str)
 }
-tribonacci([1,1,1],1);
-tribonacci([0, 0, 0],10);
-// tribonacci([1,1,1],10);
-// tribonacci([0.5,0.5,0.5],30)
+
+dashatize(274)
+dashatize(5311)
+dashatize(-5311)
+dashatize(NaN)
+dashatize(-86320)
+dashatize(0)
+dashatize(-1 )
