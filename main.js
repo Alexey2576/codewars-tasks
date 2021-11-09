2a50da21b1a45adda4f9c480b0120e3eab301631
Alexey2576 <alexeynikinitin@yandex.by>
2021-11-09 19:51:26 +0300

    Even or Odd

--- main.js
+++ main.js
@@ -1,24 +1,4 @@
-function dataReverse(data) {
-  let n = data.length/8;
-  let mass = [];
-  for (let i = 0; i < n; i++) {
-    let s = ""
-    for (let x = 0; x < 8; x++) {
-      s = s + data[x];
-    }
-    mass.push(s);
-    data = data.splice(8);
-  }
-  mass.reverse();
-  let str = "";
-  mass.forEach(el => str += el)
-  mass = str.split("")
-  return console.log(mass.map(el => parseInt(el)))
+function even_or_odd(number) {
+  return number%2 === 0 ? "Even" : "Odd"
 }
-
-const data1 = [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]
-//const data2 = [1,0,1,0,1,0,1,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1]
-dataReverse(data1)
-const data3 = [0,0,1,1,0,1,1,0,0,0,1,0,1,0,0,1]
-// const data4 = [0,0,1,0,1,0,0,1,0,0,1,1,0,1,1,0]
-dataReverse(data3)
+console.log(even_or_odd(2))
