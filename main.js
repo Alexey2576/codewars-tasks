52b383f14c9885dc0bdd6a00926cae0c1070427a
Alexey2576 <alexeynikinitin@yandex.by>
2021-11-19 15:17:22 +0300

    Beginner Series #4 Cockroach

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-function generateShape(integer){
-  let str = ""
-  for (let i = 0; i < integer; i++) {
-    for (let i = 0; i < integer; i++) {
-      str += "+"
-    }
-    str+="\n"
-  }
-  return str.substr(0, str.length-1)
+function cockroachSpeed(s) {
+  return parseInt(s * 100000 / 3600)
 }
-
-console.log(generateShape(8))
