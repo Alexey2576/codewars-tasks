e20f796a0b54dbdc64da731cb6c11079c101acc6
Alexey2576 <alexeynikinitin@yandex.by>
2021-11-23 22:06:23 +0300

    Correct the mistakes of the character recognition software

--- main.js
+++ main.js
@@ -1,19 +1,7 @@
-function balance(left,right){
-  let leftArr = left.toString().split("");
-  let rightArr = right.toString().split("");
-
-  let l = 0;
-  let r = 0;
-  for (let i = 0; i < leftArr.length; i++) {
-      leftArr[i] === "!" ? l +=2 : l+=3
-  }
-  for (let i = 0; i < rightArr.length; i++) {
-    rightArr[i] === "!" ? r += 2 : r += 3
-  }
-  if (l === r)
-    return "Balance"
-  if (l > r)
-    return "Left"
-  if (r > l)
-    return "Right"
+function correct(string)
+{
+  let s = ""
+  let mass = string.toString().split("")
+  return mass.map(t => t === "0" ? t = "O" : t === "5" ? t = "S" : t === "1" ? t = "I" : t).join("")
 }
+console.log(correct("L0ND0N"))
