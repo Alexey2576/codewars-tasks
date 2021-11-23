aadb670329a6b59dff1972ab1cb2aecbc338b45a
Alexey2576 <alexeynikinitin@yandex.by>
2021-11-23 21:27:05 +0300

    Exclamation marks series #17: Put the exclamation marks and question marks to the balance, Are they balanced?

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-function otherAngle(a, b) {
-  return 180-a-b;
+function balance(left,right){
+  let leftArr = left.toString().split("");
+  let rightArr = right.toString().split("");
+
+  let l = 0;
+  let r = 0;
+  for (let i = 0; i < leftArr.length; i++) {
+      leftArr[i] === "!" ? l +=2 : l+=3
+  }
+  for (let i = 0; i < rightArr.length; i++) {
+    rightArr[i] === "!" ? r += 2 : r += 3
+  }
+  if (l === r)
+    return "Balance"
+  if (l > r)
+    return "Left"
+  if (r > l)
+    return "Right"
 }
