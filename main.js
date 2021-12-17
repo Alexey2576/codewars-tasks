fd306041348647e9365a95dc661d00abb783f1be
Alexey2576 <alexeynikinitin@yandex.by>
2021-12-17 22:22:41 +0300

    Calculate BMI

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-function correct(string)
-{
-  let s = ""
-  let mass = string.toString().split("")
-  return mass.map(t => t === "0" ? t = "O" : t === "5" ? t = "S" : t === "1" ? t = "I" : t).join("")
+function bmi(weight, height) {
+  const bmi = weight / (height * height)
+  if (bmi <= 18.5) return "Underweight"
+
+  if (bmi <= 25.0) return "Normal"
+
+  if (bmi <= 30.0) return "Overweight"
+
+  if (bmi > 30) return "Obese"
 }
-console.log(correct("L0ND0N"))
