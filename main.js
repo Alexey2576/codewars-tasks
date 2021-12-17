578ba202764a12466f96dfa1e6928fa0abdb87d2
Alexey2576 <alexeynikinitin@yandex.by>
2021-12-17 22:36:41 +0300

    Abbreviate a Two Word Name

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function countSheeps(arrayOfSheep) {
-  let i = 0;
-  arrayOfSheep.forEach(t => t && i++)
-  return i
+function abbrevName(name){
+  const mass = name.toString().split(" ")
+  const str = mass[0].substring(0, 1).toUpperCase() + "." + mass[1].substring(0, 1).toUpperCase()
+  return str
 }
+
+console.log(abbrevName("Sam Harris"))
+console.log(abbrevName("Patrick Feenan"))
