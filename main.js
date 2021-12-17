4ece861d5316953cf252779d392c23b053cdedc0
Alexey2576 <alexeynikinitin@yandex.by>
2021-12-17 22:25:39 +0300

    Beginner Series #1 School Paperwork

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-function bmi(weight, height) {
-  const bmi = weight / (height * height)
-  if (bmi <= 18.5) return "Underweight"
-
-  if (bmi <= 25.0) return "Normal"
-
-  if (bmi <= 30.0) return "Overweight"
-
-  if (bmi > 30) return "Obese"
+function paperwork(n, m) {
+  if (n < 0 || m < 0)
+    return 0
+  else return n * m
 }
