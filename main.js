c9261c1d97a57344b6c370d4d09023d61f1aa504
Alexey2576 <alexeynikinitin@yandex.by>
2022-01-16 18:37:29 +0300

    Pirates!! Are the Cannons ready!??

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-function checkForFactor (base, factor) {
-  return base % factor === 0 && true
+const cannonsReady = (gunners) => {
+  for (let item in gunners) {
+    if (gunners[item] === 'nay')
+      return 'Shiver me timbers!'
+  }
+  return 'Fire!'
 }
 
-console.log(checkForFactor(10,2))
-console.log(checkForFactor(653,7))
+const a = {'Mike': 'aye', 'Joe': 'aye', 'Johnson': 'aye', 'Peter': 'aye'},
+  b = {'Mike': 'aye', 'Joe': 'nay', 'Johnson': 'aye', 'Peter': 'aye'};
+
+console.log(cannonsReady(a))
+console.log(cannonsReady(b))
