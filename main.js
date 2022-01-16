77e24f5e6cbc1bb3721621f4e37e53827a13fcd5
Alexey2576 <alexeynikinitin@yandex.by>
2022-01-16 19:00:16 +0300

    Rock Paper Scissors!

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
-function shortcut (string) {
-  let mass = string.toString().split('')
-  let newMass = []
-  let mass2 = ['a', 'e', 'i', 'o', 'u']
-  for (let item in mass) {
-    if (!mass2.includes(mass[item]))
-      newMass.push(mass[item])
-  }
-  return newMass.join('')
-}
+const rps = (p1, p2) => {
+  let mass = ['rock', 'scissors', 'paper']
+  let player1 = mass.indexOf(p1)
+  let player2 = mass.indexOf(p2)
+  if (player1 - player2 === -1)
+    return "Player 1 won!"
+  if (player1 - player2 === 1)
+    return "Player 2 won!"
+  if (player1 - player2 === 2)
+    return "Player 1 won!"
+  if (player1 - player2 === -2)
+    return "Player 2 won!"
+  if (player1 === player2)
+    return "Draw!"
+};
 
-console.log(shortcut('hello'))
-console.log(shortcut('how are you today?'))
-console.log(shortcut('complain'))
-console.log(shortcut('never'))
-console.log(shortcut('HELLO'))
+console.log(rps('scissors','paper'))
+console.log(rps('scissors','rock'))
+console.log(rps('paper','paper'))
