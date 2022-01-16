928cef99cc236bf2d342320fb4af7b726518ae3f
Alexey2576 <alexeynikinitin@yandex.by>
2022-01-16 19:11:14 +0300

    Holiday VIII - Duty Free

--- main.js
+++ main.js
@@ -1,19 +1,7 @@
-const rps = (p1, p2) => {
-  let mass = ['rock', 'scissors', 'paper']
-  let player1 = mass.indexOf(p1)
-  let player2 = mass.indexOf(p2)
-  if (player1 - player2 === -1)
-    return "Player 1 won!"
-  if (player1 - player2 === 1)
-    return "Player 2 won!"
-  if (player1 - player2 === 2)
-    return "Player 1 won!"
-  if (player1 - player2 === -2)
-    return "Player 2 won!"
-  if (player1 === player2)
-    return "Draw!"
-};
+function dutyFree(normPrice, discount, hol){
+  return Math.floor(hol / ( normPrice * discount / 100))
+}
 
-console.log(rps('scissors','paper'))
-console.log(rps('scissors','rock'))
-console.log(rps('paper','paper'))
+console.log(dutyFree(12, 50, 1000))
+console.log(dutyFree(17, 10, 500));
+console.log(dutyFree(24, 35, 3000));
