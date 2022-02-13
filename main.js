4fdc153641ad568ee0b20b2012196beda5b6f653
alexeynikinitin <alexeynikinitin@yandex.by>
2022-02-13 12:47:14 +0300

    Find the next perfect square!

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-function past(h, m, s){
-  return h * 3600000 + m * 60000 + s * 1000
+function findNextSquare(sq) {
+  let n = Math.sqrt(sq)
+  if(Number.isInteger(n) > 0) return Math.pow(n+1, 2)
+  return -1;
 }
+
+console.log(findNextSquare(121))
+console.log(findNextSquare(625))
+console.log(findNextSquare(319225))
+console.log(findNextSquare(15241383936))
+console.log(findNextSquare(155))
