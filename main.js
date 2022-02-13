fc89a605fd66f614301188289b0f0222a05a047a
alexeynikinitin <alexeynikinitin@yandex.by>
2022-02-13 12:25:56 +0300

    Well of Ideas - Easy Version

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-function dutyFree(normPrice, discount, hol){
-  return Math.floor(hol / ( normPrice * discount / 100))
+function well(x) {
+  let i = 0;
+  x.forEach(n => n === 'good' && i++)
+  if (i > 2) return "I smell a series!"
+  if (i > 0 && i <= 2) return "Publish!"
+  return "Fail!"
 }
 
-console.log(dutyFree(12, 50, 1000))
-console.log(dutyFree(17, 10, 500));
-console.log(dutyFree(24, 35, 3000));
+console.log(well([`bad`, 'bad', 'bad']))
+console.log(well(['good', 'bad', 'bad', 'bad', 'bad']))
+console.log(well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good']))
